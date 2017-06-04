8889da36803b1142bc30d7e68207d49d91755c45
jorgezaccaro <jorgezaccaro@gmail.com>
2017-06-04 19:59:19 +0800

    feat(scripts): improved symlink script

--- scripts/symlink.js
+++ scripts/symlink.js
@@ -6,11 +6,7 @@ const [,, lib = 'lib'] = process.argv
 
 let target = path.resolve(__dirname, '../', lib)
 let symlink = path.resolve(__dirname, '../', 'node_modules', '*' + lib)
+let nodeModules = `${__dirname}\/..\/node_modules`
 
-try {
-  fs.mkdirSync(`${__dirname}\/..\/node_modules`)
-} catch (error) {
-  if (error.code !== 'EEXIST') throw error
-} finally {
-  fs.symlinkSync(target, symlink)
-}
+if(!fs.existsSync(nodeModules)) fs.mkdirSync(nodeModules)
+if(!fs.existsSync(symlink)) fs.symlinkSync(target, symlink)
